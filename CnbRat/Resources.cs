acf7bf2f5bba8a4e65ccfa196376ad132382e8e9
Jan Hrůza <honza.hruza.d5@hotmail.com>
2025-10-07 19:08:27 +0200

    Added the Resources class.

---
+++ CnbRat/Resources.cs
@@ -0,0 +1,13 @@
+﻿namespace CnbRat;
+
+/// <summary>
+/// Representing application resources.
+/// </summary>
+public static class Resources
+{
+    /// <summary>
+    /// Representing an URL address pointing to the CNB exchange rates CSV file.
+    /// This link has no parameters - it fetches the current exchange rates.
+    /// </summary>
+    public const string URL_EXCHANGE_RATES = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt";
+}
