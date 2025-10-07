c0f534062e3b973cfd12497e4d390d8b98fbd936
Jan Hrůza <honza.hruza.d5@hotmail.com>
2025-10-07 19:05:16 +0200

    Added a new project: CnbRat

---
+++ CnbRat/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+internal class Program
+{
+    private static int Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            Console.Error.WriteLine("Arguments are disabled.");
+            return 1;
+        }
+
+        return 0;
+    }
+}
